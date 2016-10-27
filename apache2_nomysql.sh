9c2ed4bdea45f4c27dd4505fe3090c0cddd26fc3
Ben "thekumpel <thekumpelhd@gmail.com>
2016-10-27 15:53:38 +0200

    Create apache2_nomysql.sh

---
+++ apache2_nomysql.sh
@@ -0,0 +1,14 @@
+clear
+echo Updatating Packages!
+apt-get update && apt-get upgrade -y
+clear
+echo Installing Apache2!
+apt-get install apache2 php5 libapache2-mod-php5
+/etc/init.d/apache2 restart
+clear
+echo Apache2 ohne MSQL erfolgreich installiert!
+rm -r apache2_nomysql.sh
+
+
+
+
