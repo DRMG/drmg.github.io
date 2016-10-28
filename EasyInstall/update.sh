efa5c571f7559d8d4581f255573c78888168f3d1
Ben "thekumpel <thekumpelhd@gmail.com>
2016-10-28 15:22:04 +0200

    Update update.sh

--- EasyInstall/update.sh
+++ EasyInstall/update.sh
@@ -1,7 +1,8 @@
+rm -r easy_inst.sh
 echo "Removing old Files"
 rm -R /etc/drmg/
 wget https://drmg.github.io/EasyInstall/easy_inst.sh
 chmod -R 777 installer.sh
-sh installer.sh
+sh easy_inst.sh
 clear
 echo "Updated!"
