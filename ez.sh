b5149e8d5201fa839746353a74488dce611fe723
Ben "thekumpel <thekumpelhd@gmail.com>
2016-10-28 15:24:37 +0200

    Create ez.sh

---
+++ ez.sh
@@ -0,0 +1,8 @@
+rm -r easy_inst.sh
+echo "Removing old Files"
+rm -R /etc/drmg/
+wget https://drmg.github.io/EasyInstall/easy_inst.sh
+chmod -R 777 easy_inst.sh
+sh easy_inst.sh
+clear
+echo "Updated!"
