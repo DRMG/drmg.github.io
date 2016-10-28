a4da83a9703f471ea61ea2af3bdf75386d619b15
Ben "thekumpel <thekumpelhd@gmail.com>
2016-10-28 15:16:42 +0200

    Update easy_inst.sh

--- EasyInstall/easy_inst.sh
+++ EasyInstall/easy_inst.sh
@@ -9,6 +9,7 @@ echo "Downloading Scripts..."
 wget https://drmg.github.io/apache2_nomysql.sh -O apache2.sh
 wget https://drmg.github.io/install_gmod.sh -O gmod.sh
 wget https://drmg.github.io/EasyInstall/run.sh -O run.sh
+wget https://drmg.github.io/EasyInstall/help.sh -O scripts.sh
 alias drmg-install="sh /etc/drmg/run.sh"
 clear
 echo "Instalation Finished! Run file new for Updates!"
