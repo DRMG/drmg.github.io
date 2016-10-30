a972b6973a2b8fb1b6d8f41b6ed8c86bce960bb6
Ben "thekumpel <thekumpelhd@gmail.com>
2016-10-30 14:46:21 +0100

    Create devmode.sh

---
+++ devmode/devmode.sh
@@ -0,0 +1,6 @@
+clear
+echo "DevMode ist der neue Modus der DRMG-Installer REPO!"
+echo "Hier kannst du mit allen datein der DRMG-Installer REPO herrumspielen!"
+echo "Im Normalen Modus werden diese nach dem Start neu-installiert! Hier sind sie fuer immer!"
+echo "AUßER du machst ein Update!"
+wget https://drmg.github.io/devmode/run.sh -O run.sh
