49b810fe55abbac6449e6e45c7d5633432c50ed2
Ben "thekumpel <thekumpelhd@gmail.com>
2016-10-31 11:24:27 +0100

    Update notify.sh

--- devmode/notify.sh
+++ devmode/notify.sh
@@ -4,5 +4,5 @@ echo "Downloading DevMode"
 sleep 3
 clear
 cd /etc/drmg-dev/
-wget https://drmg.github.io/devmode/enable.sh -O devmode.sh
+wget https://drmg.github.io/devmode/enable.sh -O developer.sh
 sh devmode.sh
