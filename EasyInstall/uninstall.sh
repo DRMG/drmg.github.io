f78d0de7c3aac16e4eca937406c46160c2956a52
Ben "thekumpel <thekumpelhd@gmail.com>
2016-11-07 08:54:42 +0100

    Update uninstall.sh

--- EasyInstall/uninstall.sh
+++ EasyInstall/uninstall.sh
@@ -4,6 +4,9 @@ sleep 3
 cd /etc/drmg
 rm *
 rmdir /etc/drmg
+cd /etc/drmg-dev
+rm *
+rmdir /etc/drmg-dev
 clear
 echo "Der DRMG-Installer wurde entfernt!"
 echo "Schaue auf unser Webseite vorbei: http://davidrobin.qloc-hosting.de"
