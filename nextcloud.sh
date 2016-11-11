4cb69fd0d1a743e3df8ab201f033e1e008ddf3c4
Ben "thekumpel <thekumpelhd@gmail.com>
2016-11-11 17:26:43 +0100

    Update nextcloud.sh

--- nextcloud.sh
+++ nextcloud.sh
@@ -3,7 +3,7 @@ echo Updatating and Installing Packages!
 apt-get update && apt-get upgrade && apt-get install php5 php5-gd php-xml-parser php5-intl php5-sqlite php5-mysql php5-curl php5-mcrypt curl libcurl3 -y
 clear
 echo Installing Nextcloud!
-wget https://github.io/DRMG/drmg.github.io/setup_nextcloud.php
+wget https://download.nextcloud.com/server/installer/setup-nextcloud.php
 mv setup_nextcloud.php /var/www/html/
 chmod 777 *
 clear
