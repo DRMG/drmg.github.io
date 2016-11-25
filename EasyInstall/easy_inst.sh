6291bec9f630b499922a00f1d0c648f8f8c660d1
Ben "thekumpel <thekumpelhd@gmail.com>
2016-11-25 17:57:38 +0100

    Update easy_inst.sh

--- EasyInstall/easy_inst.sh
+++ EasyInstall/easy_inst.sh
@@ -7,7 +7,7 @@ clear
 echo "Directory Created!"
 echo "Downloading Scripts..."
 wget https://drmg.github.io/apache2_nomysql.sh -O apache2.sh
-wget https://drmg.github.io/install_gmod.sh -O gmod.sh
+#wget https://drmg.github.io/install_gmod.sh -O gmod.sh
 wget https://drmg.github.io/EasyInstall/run.sh -O run.sh
 wget https://drmg.github.io/EasyInstall/scripts.sh -O scripts.sh
 wget https://drmg.github.io/update.sh -O update.sh
@@ -15,7 +15,7 @@ wget https://drmg.github.io/start.sh -O programm
 wget https://drmg.github.io/EasyInstall/uninstall.sh -O uninstall.sh
 wget https://drmg.github.io/whoisconnected.sh -O whoisconnected.sh
 wget https://drmg.github.io/nextcloud.sh -O nextcloud.sh
-wget https://drmg.github.io/steamcmd.sh -O steamcmd.sh
+#wget https://drmg.github.io/steamcmd.sh -O steamcmd.sh
 alias drmg-install="sh /etc/drmg/run.sh"
 chmod -R 777 /etc/drmg/
 clear
