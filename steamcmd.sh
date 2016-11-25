6f742c416fb8f7e8284d7f867b748de5525bd83a
David Robin <TheDavidRobin@users.noreply.github.com>
2016-11-25 17:48:43 +0100

    Update steamcmd.sh

--- steamcmd.sh
+++ steamcmd.sh
@@ -6,6 +6,6 @@ mkdir bin
 cd bin
 wget http://media.steampowered.com/client/steamcmd_linux.tar.gz
 tar -xvzf steamcmd_linux.tar.gz
-./steamcmd.sh +login anonymous +quit
+sh steamcmd.sh +login anonymous +quit
 clear
 echo SteamCMD erfolgreich installiert!
