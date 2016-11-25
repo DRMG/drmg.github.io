edc8276779577a80f665cdd0f0651e2c23ba2c5f
Ben "thekumpel <thekumpelhd@gmail.com>
2016-11-25 17:40:02 +0100

    Update run.sh

--- EasyInstall/run.sh
+++ EasyInstall/run.sh
@@ -1,6 +1,7 @@
 version="1.0"
 developers="David,Ben"
 downloadserver="https://drmg.github.io/ez.sh"
+mode="normal"
 wget $downloadserver -O update.sh
 sh update.sh
 rm -r update.sh
@@ -8,6 +9,10 @@ cd /etc/drmg/
 #wget https://drmg.github.io/devmode/notify.sh -O developer.sh
 clear
 sh /etc/drmg/scripts.sh
+if ["$mode" == "developer"]
+then
+  echo "You are in Developer Mode!"
+fi
 echo "Was installieren?"
 read -p "" var1
 sh /etc/drmg/$var1.sh
