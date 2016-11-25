285ef77283b0ec68509b5fd841b05f003630a45e
Ben "thekumpel <thekumpelhd@gmail.com>
2016-11-25 17:53:36 +0100

    Update run.sh

--- EasyInstall/run.sh
+++ EasyInstall/run.sh
@@ -1,20 +1,15 @@
 version="1.0"
+branch="1.0"
+versiontype="Stable"
 developers="DRMG"
 downloadserver="https://drmg.github.io/ez.sh"
-mode="normal"
-devmode="devmode"
-
-if [ $mode = $devmode ]
-then
-        sed s/downloadserver="https://drmg.github.io/ez.sh"/downloadserver=" "/g run.sh 
-fi
-
 wget $downloadserver -O update.sh
 sh update.sh
 rm -r update.sh
 cd /etc/drmg/
 #wget https://drmg.github.io/devmode/notify.sh -O developer.sh
 clear
+echo "Using Version $version $version from §developers."
 sh /etc/drmg/scripts.sh
 echo "Was installieren?"
 read -p "" var1
