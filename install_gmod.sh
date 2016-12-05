5b296b47b9907c1b32d5e2844eeb0c8fb73168d6
David Robin <TheDavidRobin@users.noreply.github.com>
2016-12-05 17:06:51 +0100

    Update install_gmod.sh

--- install_gmod.sh
+++ install_gmod.sh
@@ -4,7 +4,7 @@ cd bin
 wget http://media.steampowered.com/client/steamcmd_linux.tar.gz
 tar -xvzf steamcmd_linux.tar.gz
 sh steamcmd.sh +login anonymous +quit
-wget https://drmg.github.io/update_gmod.sh -O update_gmod.sh
+wget http://pastebin.com/raw/Mw1m8USc -O update_gmod.sh
 chmod +x update_gmod.sh
 apt-get install dos2unix
 dos2unix update_gmod.sh
