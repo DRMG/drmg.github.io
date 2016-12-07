02a4c9a89dab701fe64e778cc4ff89786be373f1
Ben "thekumpel <thekumpelhd@gmail.com>
2016-12-07 20:13:22 +0100

    Create commingsoon.sh

---
+++ api/commingsoon.sh
@@ -0,0 +1,3 @@
+echo "The DRMG-API is still in Development!"
+echo "It will be comming with DRMG Version 2.0"
+echo "More Informations for V2.0 here: http://davidrobin.qloc-hosting.de/blog/drmg-installer-todo/"
