8fd1f729f007fa75805b240697824096a34f12ac
Ben "thekumpel <thekumpelhd@gmail.com>
2017-05-02 14:12:18 +0200

    Update bungee.sh

--- bungee.sh
+++ bungee.sh
@@ -2,8 +2,8 @@ clear
 apt-get update && apt-get upgrade -y
 apt-get install openjdk-7-jre-headless screen nano -y
 cd /home
-mkdir Server1
-cd Server1/
+mkdir ServerB
+cd ServerB/
 wget https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target/BungeeCord.jar -O spigot_server.jar
 echo -n > start.sh
 echo "screen -S ServerB java -Xmx500M -Xms500M -jar spigot_server.jar" > /home/Server1/start.sh
