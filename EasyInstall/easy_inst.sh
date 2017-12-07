c9b38d0ea8eac356b02d4153ce85b54b8ad3f6f0
Ben "thekumpel <thekumpelhd@gmail.com>
2017-12-07 18:16:12 +0100

    Update easy_inst.sh

--- EasyInstall/easy_inst.sh
+++ EasyInstall/easy_inst.sh
@@ -7,17 +7,17 @@ cd /etc/drmg
 clear
 echo "Directory Created!"
 echo "Downloading Scripts..."
-wget https://drmg.github.io/apache2_nomysql.sh -O apache2.sh
-#wget https://drmg.github.io/install_gmod.sh -O gmod.sh
-wget https://drmg.github.io/EasyInstall/run.sh -O run.sh
-wget https://drmg.github.io/EasyInstall/scripts.sh -O scripts.sh
-wget https://drmg.github.io/update.sh -O update.sh
-wget https://drmg.github.io/start.sh -O programm
-wget https://drmg.github.io/EasyInstall/uninstall.sh -O uninstall.sh
-wget https://drmg.github.io/whoisconnected.sh -O whoisconnected.sh
-wget https://drmg.github.io/nextcloud.sh -O nextcloud.sh
-#wget https://drmg.github.io/steamcmd.sh -O steamcmd.sh
-wget https://drmg.github.io/mc.sh -O mc-server.sh
+wget https://drmg.github.io/apache2_nomysql.sh -O apache2.sh -q
+#wget https://drmg.github.io/install_gmod.sh -O gmod.sh -q
+wget https://drmg.github.io/EasyInstall/run.sh -O run.sh -q
+wget https://drmg.github.io/EasyInstall/scripts.sh -O scripts.sh -q
+wget https://drmg.github.io/update.sh -O update.sh -q
+wget https://drmg.github.io/start.sh -O programm -q
+wget https://drmg.github.io/EasyInstall/uninstall.sh -O uninstall.sh -q
+wget https://drmg.github.io/whoisconnected.sh -O whoisconnected.sh -q
+wget https://drmg.github.io/nextcloud.sh -O nextcloud.sh -q
+#wget https://drmg.github.io/steamcmd.sh -O steamcmd.sh -q
+wget https://drmg.github.io/mc.sh -O mc-server.sh -q
 alias drmg-install="sh /etc/drmg/run.sh"
 chmod -R 777 /etc/drmg/
 clear
