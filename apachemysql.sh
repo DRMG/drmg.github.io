01fd6e814fcc66ffd4e16ddd72422fb5d95fe50d
Ben "thekumpel <thekumpelhd@gmail.com>
2019-03-02 15:14:38 +0100

    Create apachemysql.sh

---
+++ apachemysql.sh
@@ -0,0 +1,10 @@
+apt-get install mysql-server
+apt-get install apache2 libapache2-mod-php5 php5 php5-mysql php5-mcrypt
+cd /var/www/html
+wget https://files.phpmyadmin.net/phpMyAdmin/4.6.0/phpMyAdmin-4.6.0-all-languages.tar.gz -O phpMyAdmin.tar.gz
+tar -xzvf phpMyAdmin.tar.gz
+rm phpMyAdmin.tar.gz
+mv phpMyAdmin-*/ phpMA/
+chown -R www-data:www-data /var/www/html/phpMA
+clear
+echo "Fertig!"
